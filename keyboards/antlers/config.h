6e8bf4b9c8a33d52658145dd1b55daa584c6c8ec
elkowar <5300871+elkowar@users.noreply.github.com>
2022-07-06 18:23:44 +0200

    add antlers keyboard

---
+++ keyboards/antlers/config.h
@@ -0,0 +1,49 @@
+/* Copyright 2022 ElKowar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 6
+
+// many things are specified in info.json
+
+#define UNUSED_PINS \
+    {}
+
+/* set to 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+#define QMK_KEYS_PER_SCAN 4
+#define USB_POLLING_INTERVAL_MS 1
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
